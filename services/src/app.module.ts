77a4e9da30ad72f4cccc342bfe99eda59df8b53b
Sherly1001 <longlepkd2000@gmail.com>
2022-11-29 01:25:20 +0700

    fix: config db

--- services/src/app.module.ts
+++ services/src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import config from './config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppConfig, appConfig } from './config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [config] })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [appConfig] }),
+    MongooseModule.forRootAsync({
+      inject: [appConfig.KEY],
+      useFactory: (cfg: AppConfig) => {
+        return {
+          uri: cfg.database.uri,
+          dbName: cfg.database.name,
+        };
+      },
+    }),
+  ],
 })
 export class AppModule {}
--- services/src/main.ts
+++ services/src/main.ts
@@ -1,20 +1,19 @@
 import { Logger } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { AppConfig } from './config';
+import { appConfig, AppConfig } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService<AppConfig>);
+  const cfg = app.get<AppConfig>(appConfig.KEY);
 
   const doc = new DocumentBuilder().setTitle('Blue Dot Api').build();
 
   const document = SwaggerModule.createDocument(app, doc);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(configService.get('port'));
+  await app.listen(cfg.port);
   Logger.log('Application runing at ' + (await app.getUrl()));
 }
 bootstrap();
