7d682b537648b1d67e6afd40318512bf4c2a35e8
Sherly1001 <longlepkd2000@gmail.com>
2023-02-01 15:23:35 +0700

    fix: delete item's shop

--- services/src/api/shop/shop.module.ts
+++ services/src/api/shop/shop.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Shop, ShopSchema } from 'src/domain/schemas';
+import { Item, ItemSchema, Shop, ShopSchema } from 'src/domain/schemas';
 import { UserModule } from '../user/user.module';
 import { ShopController } from './shop.controller';
 import { ShopService } from './shop.service';
@@ -8,7 +8,10 @@ import { ShopService } from './shop.service';
 @Module({
   imports: [
     UserModule,
-    MongooseModule.forFeature([{ name: Shop.name, schema: ShopSchema }]),
+    MongooseModule.forFeature([
+      { name: Shop.name, schema: ShopSchema },
+      { name: Item.name, schema: ItemSchema },
+    ]),
   ],
   providers: [ShopService],
   controllers: [ShopController],
--- services/src/api/shop/shop.service.ts
+++ services/src/api/shop/shop.service.ts
@@ -2,13 +2,14 @@ import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { FilterQuery, Model } from 'mongoose';
 import { BaseResult } from 'src/domain/dtos/base.result';
-import { Shop, ShopDocument } from 'src/domain/schemas';
+import { Item, ItemDocument, Shop, ShopDocument } from 'src/domain/schemas';
 import { UpdateShopDto } from './dtos';
 
 @Injectable()
 export class ShopService {
   constructor(
     @InjectModel(Shop.name) private readonly shopModel: Model<ShopDocument>,
+    @InjectModel(Item.name) private readonly itemModel: Model<ItemDocument>,
   ) {}
 
   async getShops(search?: string) {
@@ -53,7 +54,33 @@ export class ShopService {
 
   async deleteShop(id: string) {
     const result = new BaseResult<Shop>();
-    result.data = await this.shopModel.findOneAndDelete({ _id: id });
+
+    const session = await this.shopModel.startSession();
+
+    try {
+      session.startTransaction();
+      result.data = await this.shopModel.findOneAndDelete(
+        { _id: id },
+        { session },
+      );
+
+      await this.itemModel.updateMany(
+        { 'prices.shop': id },
+        {
+          $pull: {
+            prices: { shop: id },
+          },
+        },
+        { session },
+      );
+
+      await session.commitTransaction();
+    } catch (err) {
+      await session.abortTransaction();
+      throw new UnprocessableEntityException(err.toString());
+    } finally {
+      await session.endSession();
+    }
     return result;
   }
 }
